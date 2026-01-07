dae79327ac25a499439f2c9f79ab8e1188c19314
Austin Nelson <nelsonhaustin@gmail.com>
2026-01-06 21:30:09 -0800

    use new sqllite service

--- services/backend/internal/db/sqlite.go
+++ services/backend/internal/db/sqlite.go
@@ -2,15 +2,27 @@ package db
 
 import (
 	"database/sql"
+	"os"
+	"path/filepath"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 func NewSqLiteDb(path string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", path)
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
+	}
+
+	dsn := "file:" + path + "?_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)"
+
+	db, err := sql.Open("sqlite", dsn)
 	if err != nil {
 		return nil, err
 	}
+
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
